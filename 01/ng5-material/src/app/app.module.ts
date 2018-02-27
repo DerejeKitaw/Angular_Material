c5ff465cc0f069a38eddcd7cc7675ec5beb14a39
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-02-27 00:06:16 -0500

    import HttpClientModule

--- 01/ng5-material/src/app/app.module.ts
+++ 01/ng5-material/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { MaterialModule } from './material/material.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
